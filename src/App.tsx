6182cd64a08510e4e52657a6013135a7fc2c817d
Yuliya K <yuli-kretova@yndex.ru>
2023-11-08 22:27:36 +0300

    add api, search bar, weather logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import './App.scss';
 import ghLogo from './assets/icons/footer/icons8-github-100.svg'
+import SearchBar from './SearchBar/SearchBar';
+import { WeatherLogo } from './WeatherLogo/WeatherLogo';
 
 function App() {
 
   return (
     <div className="App-layout">
-      <div className='content'>Content</div>
+      <div className='content'>
+        <WeatherLogo />
+        <SearchBar />
+      </div>
       <div className='footer'>
-        <a href="https://github.com/Averah" target="_blank">
+        <a href="https://github.com/Averah" target="_blank" rel="noopener noreferrer">
           <img className='gh-logo' src={ghLogo} alt='' />
         </a>
       </div>
